77b3b8221429294a9a525d4776819ca9d6ed637c
vierageorge <joviera@gmail.com>
2021-01-31 12:47:07 -0500

    Rename appender variable in utils

--- smsprocessor/common/utils.py
+++ smsprocessor/common/utils.py
@@ -4,7 +4,7 @@ from .exceptions import NotValidExtension, NotValidMimeType
 from dateutil.parser import parse
 
 
-def process_file(drive_service, file, appender):
+def process_file(drive_service, file, sheet_appender):
     file_name = file['name']
     if file['fileExtension'] not in VALID_EXTENSIONS:
         raise NotValidExtension(file_name)
@@ -14,5 +14,5 @@ def process_file(drive_service, file, appender):
     (sender, timestamp_s, message) = drive_service.get_file_content(file['id']).split('|')
     date_s = f'{file_name[0:4]}/{file_name[4:6]}/{file_name[6:8]}'
     date = parse(date_s, fuzzy=False)
-    appender.append_to_sheet(date_s, message)
+    sheet_appender.append_to_sheet(date_s, message)
     drive_service.change_parent_directory(file['id'], SOURCE_FOLDER_ID, PROCESSED_FOLDER_ID)
